e015d9beae6bfacb5b453125909a05e2d0df94f3
Valery Nosareu <valery.nosareu@gmail.com>
2022-08-07 23:07:24 +0300

    init: initial branch

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./build/webpack.base');
+const devConfig = require('./build/webpack.dev');
+const prodConfig = require('./build/webpack.prod');
+
+module.exports = (env, argv) => {
+  switch (argv.mode) {
+    case 'development':
+      return merge(baseConfig, devConfig);
+    case 'production':
+      return merge(baseConfig, prodConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+};
+
+// cross-env NODE_ENV=development webpack-dev-server --config ./build/webpack.base.js --open для сервера
