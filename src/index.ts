b5f79e56a303946a362e71da0db8c87be60e18e3
Valery Nosareu <valery.nosareu@gmail.com>
2022-08-16 16:40:25 +0300

    doc: ref code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
 import './view/styles/common.scss';
-
-const tests = (): void => {
-  console.log('Hello World');
-};
-
-tests();
-
-const a: string = 'asd-asd';
-
-console.log(a.charCodeAt(2));
-
-const b = (name: string): string => `Hello ${name}`;
-
-console.log(b(a));
