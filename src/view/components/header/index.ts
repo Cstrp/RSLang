896d8249fc66ff12e53c2d0aaa1c1e5955569fda
Valery Nosareu <valery.nosareu@gmail.com>
2022-08-18 20:11:48 +0300

    feat: add footer, main without content

--- src/view/components/header/index.ts
+++ src/view/components/header/index.ts
@@ -5,23 +5,25 @@ import {Clock} from '@/data/utils/_clock';
 class Header extends Template {
   constructor(parent: HTMLElement) {
     super(parent, 'header', style.header);
-    new Clock(this.element);
+
     new Template(this.element, 'div', style.greeting, `${this.greeting()}`);
   }
 
   greeting(): string {
-    const date = new Date();
+    const date: Date = new Date();
     const time: number = date.getHours();
 
     let greeting: string = '';
 
     if (time < 12) {
-      greeting = 'Good morning!';
+      greeting = 'Good morning! Перевод: Доброе утро!';
     } else if (time >= 12 && time <= 17) {
-      greeting = 'Have a good day mate!';
-    } else if (time >= 17 && time <= 24) {
-      greeting = 'Good night! Good luck!';
-    }
+      greeting = 'Have a good day mate! Перевод: "Хорошего дня приятель!"';
+    } else if (time >= 17 && time <= 22) {
+      greeting = 'Good evening! Перевод: "Добрый вечер"';
+    } else if (time <= 23) greeting = 'Good night! Good luck!';
+
+    new Clock(this.element);
 
     return greeting;
   }
