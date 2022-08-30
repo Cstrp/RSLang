f8a3a989f56fb36556cdc227367bb4388c14abd4
Valery Nosareu <valery.nosareu@gmail.com>
2022-08-30 14:43:27 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
 import './view/styles/common.scss';
-import axios from 'axios';
-
-// example Server usage
-// https://rslang-bc.herokuapp.com/doc/ - documentations
-
-axios.get('https://rslang-bc.herokuapp.com/words').then((res) => {
-  console.log(res.data); // ?page=2&group=0
-}); // words
-
-axios.get('https://rslang-bc.herokuapp.com/files/01_0001.jpg').then((res) => {
-  console.log(res);
-}); // image
-
-axios.get('https://rslang-bc.herokuapp.com/files/16_3320.mp3').then((res) => {
-  console.log(res);
-}); // mp3 files
